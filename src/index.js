fc80799b9140b7ec02a7a60a2cb1e246ff8717f7
sidpd51 <sidpd510@gmail.com>
2024-10-17 20:11:30 +0530

    feat: Add create functionality in user-service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const { PORT } = require("./config/serverConfig.js");
-
 const express = require("express");
+
+const { PORT } = require("./config/serverConfig.js");
+const ApiRoutes = require('./routes/index.js')
+
 
 const setupAndStartServer = () => {
     const app = express();
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use('/api',ApiRoutes)
 
     app.listen(PORT, () => {
         console.log(`server is listening on port no: ${PORT}`);
