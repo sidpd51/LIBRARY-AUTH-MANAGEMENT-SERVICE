221c2577e94e8b19c6245d32c1753393145fc739
sidpd51 <sidpd510@gmail.com>
2024-10-17 23:28:13 +0530

    feat: Add get user by id functionality in user-repository

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,30 +1,41 @@
-
 const { User } = require("../models/index");
 
 class UserRepository {
-    async create(data){
+    async create(data) {
         try {
-            const user = await User.create(data)
-            return user
+            const user = await User.create(data);
+            return user;
         } catch (error) {
-            console.log("something went wrong in repository layer")
-            throw error
+            console.log("something went wrong in repository layer");
+            throw error;
         }
     }
 
-    async destroy(userId){
+    async destroy(userId) {
         try {
             await User.destroy({
                 where: {
-                    id: userId
-                }
-            })
-            return true
+                    id: userId,
+                },
+            });
+            return true;
         } catch (error) {
-            console.log("something went wrong in repository layer")
-            throw error
+            console.log("something went wrong in repository layer");
+            throw error;
+        }
+    }
+
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ["email", "id"],
+            });
+            return user;
+        } catch (error) {
+            console.log("something went wrong in repository layer");
+            throw error;
         }
     }
 }
 
-module.exports = UserRepository
+module.exports = UserRepository;
