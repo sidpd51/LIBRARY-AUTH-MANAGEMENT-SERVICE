4407edc5412c45c55d6352f02c7c65139b690dfe
sidpd51 <sidpd510@gmail.com>
2024-10-18 00:16:53 +0530

    Add checkPassword functionality to user repository

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const UserRepository = require("../repositories/user-repository.js");
 const { JWT_KEY } = require("../config/serverConfig.js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 class UserService {
     constructor() {
@@ -20,19 +21,31 @@ class UserService {
     createToken(user) {
         try {
             const result = jwt.sign(user, JWT_KEY, { expiresIn: "5d" });
-            return result
+            return result;
         } catch (error) {
             console.log("something went wrong while token creation");
             throw error;
         }
     }
 
-    verifyToken(token){
+    verifyToken(token) {
         try {
-            const response = jwt.verify(token,JWT_KEY)
-            return response
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
         } catch (error) {
-            console.log("something went wrong while token verification",error);
+            console.log("something went wrong while token verification", error);
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(
+                userInputPlainPassword,
+                encryptedPassword
+            );
+        } catch (error) {
+            console.log("something went wrong while password verification");
             throw error;
         }
     }
