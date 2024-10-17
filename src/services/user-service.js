4af9302131ef00f273c09a022c546157b5289d70
sidpd51 <sidpd510@gmail.com>
2024-10-17 23:55:53 +0530

    feat: Add createToken and verifyToken functionality to user-repository

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,19 +1,41 @@
-const UserRepository = require('../repositories/user-repository.js')
+const UserRepository = require("../repositories/user-repository.js");
+const { JWT_KEY } = require("../config/serverConfig.js");
+const jwt = require("jsonwebtoken");
 
 class UserService {
-    constructor(){
-        this.userRepository =new UserRepository()
+    constructor() {
+        this.userRepository = new UserRepository();
     }
 
-    async create(data){
+    async create(data) {
         try {
-           const user = await this.userRepository.create(data);
-           return user; 
+            const user = await this.userRepository.create(data);
+            return user;
         } catch (error) {
-            console.log("something went wrong in the service layer")
-            throw error
+            console.log("something went wrong in the service layer");
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: "5d" });
+            return result
+        } catch (error) {
+            console.log("something went wrong while token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token,JWT_KEY)
+            return response
+        } catch (error) {
+            console.log("something went wrong while token verification",error);
+            throw error;
         }
     }
 }
 
-module.exports = UserService
+module.exports = UserService;
